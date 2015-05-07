f036dab28a71e8bbf9e10bb575e401d98eaf8ea9
Higuchi Naoya <n-higuchi@cn-door.com>
2015-05-07 19:36:41 +0900

    仕事終わり

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,30 @@
 <div class="wrap">
 	<h2>クチコミ</h2>
 	<form id="kuchikomi-form" method="post" action="">
-		<?php wp_nonce_field( 'my-nonce-key', 'kuchikomi' ); ?>
+		<?php wp_nonce_field( 'kuchikomi-key', 'kuchikomi-chk' ); ?>
+		
+		<?php $options = get_option( 'kuchikomi_options' ); ?>
+		<?php for ( $i = 0; $i <= count($options); $i++ ) { ?>
+			<h3 class="title">ITEM<?php echo $i; ?></h3>
+			<p>
+				タイプ
+				<select name="kuchikomi_custom[<?php echo $i; ?>][type]">
+					<option value="">選択してください</option>
+					<option value="text" <?php echo $this->is_selected($options, $i, 'type', 'text'); ?>>テキスト</option>
+					<option value="select" <?php echo $this->is_selected($options, $i, 'type', 'select'); ?>>セレクトボックス</option>
+					<option value="checkbox" <?php echo $this->is_selected($options, $i, 'type', 'checkbox'); ?>>チェックボックス</option>
+					<option value="rating" <?php echo $this->is_selected($options, $i, 'type', 'rating'); ?>>評価</option>
+				</select>
+			</p>
+<?php /* ?>
 		<p>
-			タイトル<input type="text" name="kuchikomi" value="<?php echo esc_attr( get_option( 'test' ) ); ?>" />
+			スラッグ<input type="text" name="slug[]" value="<?php echo esc_attr( get_option( 'slug[]' ) ); ?>" />
 		</p>
+		<p>
+			ラベル<input type="text" name="title[]" value="<?php echo esc_attr( get_option( 'slug[]' ) ); ?>" />
+		</p>
+<?php */ ?>
+		<?php } ?>
 		<p>
 			<input type="submit" value="<?php echo esc_attr( __( 'Save', 'default' ) ); ?>" class="button button-primary button-large" />
 		</p>
--- kuchikomi.php
+++ kuchikomi.php
@@ -13,38 +13,100 @@ Domain Path: /languages
 class kuchikomi {
 
 	public function __construct() {
-		add_filter( 'comment_form_defaults', array( $this, 'kuchikomi_comment_form_defaults') );
-		add_filter( 'comment_form_field_comment', array( $this, 'kuchikomi_comment_form_field_comment') );
-		add_filter( 'comment_form_submit_button', array( $this, 'kuchikomi_comment_form_submit_button'), 10, 2 );
-		add_action( 'admin_menu', array( $this, 'my_plugin_menu') );
+		add_filter( 'comment_form_defaults', array( $this, 'comment_form_defaults') );
+		add_filter( 'comment_form_field_comment', array( $this, 'comment_form_field_comment') );
+		add_filter( 'comment_form_submit_button', array( $this, 'comment_form_submit_button'), 10, 2 );
+		add_action( 'admin_menu', array( $this, 'admin_menu') );
+		add_action( 'admin_init', array( $this, 'admin_init') );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts') );
 	}
 
-	public function my_plugin_menu () {
+	public function admin_menu () {
 		add_options_page('kuchikomi', 'kuchikomi', 'manage_options', basename(__FILE__), array( $this, 'admin_page') );
 	}	
+
+	public function admin_init () {
+		if ( isset( $_POST['kuchikomi-chk'] ) && $_POST['kuchikomi-chk'] ){
+			if ( check_admin_referer( 'kuchikomi-key', 'kuchikomi-chk' ) ){
+				$options = array();
+				$ii = 0;
+				for($i = 0; $i < count($_POST['kuchikomi_custom']); $i++) {
+					if ( isset( $_POST['kuchikomi_custom'][$i]['type'] ) && $_POST['kuchikomi_custom'][$i]['type'] ){
+						$options[$ii]['type'] = $_POST['kuchikomi_custom'][$i]['type'];
+						$ii++;
+					}
+				}
+				update_option( 'kuchikomi_options', $options );
+
+				//f5対策
+				wp_safe_redirect( menu_page_url( 'kuchikomi', false ) );
+			}
+		}
+	}	
+
+	public function admin_enqueue_scripts( $hook ) {
+		wp_enqueue_script(
+			'kuchikomi_admin.js',
+			plugins_url( 'js/kuchikomi_admin.js', __FILE__ ),
+			array( 'jquery' ),
+			'0.1',
+			false
+		);
+	}
 
 	public function admin_page() {
 		include(dirname(__FILE__).'/admin/index.php');
 	}
 
-	public function test() {
-		echo 'test';
+	public function is_selected($options, $i, $element, $tgt_value) {
+		$return = '';
+		if ( isset($options[$i][$element]) ) {
+			if ( $options[$i][$element] == $tgt_value ) {
+				$return = 'selected="selected"';
+			}
+		}
+		return $return;
 	}
 
-	public function kuchikomi_comment_form_defaults( $defaults ) {
+	public function comment_form_defaults( $defaults ) {
 		$defaults['title_reply'] = 'クチコミをのこす';
 		return $defaults;
 	}
 
-	public function kuchikomi_comment_form_field_comment( $comment ) {
+	public function comment_form_field_comment( $comment ) {
 		$comment = '
 			<p class="comment-form-title">
-			<label for="title">タイトル</label>
-			<input id="title" name="title" type="text" value="" size="30" />
-			</p>'.$comment;
+				<label for="title">タイトル</label>
+				<input id="title" name="title" type="text" value="" size="30" />
+			</p>
+			<p class="comment-form-rating">
+				<label for="rating">評価</label>
+				<span class="stars">☆☆☆☆☆</span>
+			</p>
+			<p class="comment-form-type">
+				<label for="type">肌質</label>
+				<select id="type" name="type">
+					<option value="乾燥肌">乾燥肌</option>
+					<option value="脂性肌・オイリー肌">脂性肌・オイリー肌</option>
+					<option value="敏感肌">敏感肌</option>
+					<option value="混合肌">混合肌</option>
+					<option value="アトピー肌">アトピー肌</option>
+					<option value="ニキビ肌">ニキビ肌</option>
+					<option value="デリケート肌">デリケート肌</option>
+					<option value="かさかさ肌">かさかさ肌</option>
+					<option value="秋肌">秋肌</option>
+					<option value="枯れ肌">枯れ肌</option>
+					<option value="年齢肌">年齢肌</option>
+				</select>
+			</p>
+			<p class="comment-form-age">
+				<label for="age">年齢</label>
+				<input id="age" name="age" type="number" value="" size="3" />歳
+			</p>'
+			.$comment;
 		return $comment;
 	}
-	public function kuchikomi_comment_form_submit_button( $submit_button, $args ) {
+	public function comment_form_submit_button( $submit_button, $args ) {
 		$submit_button = sprintf(
 			$args['submit_button'],
 			esc_attr( $args['name_submit'] ),
